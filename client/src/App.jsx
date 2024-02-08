fc3a55ce01dff6d46316323d87f4d55b1d238149
Serhii Bordeniuk <brdnk.srh@gmail.com>
2024-02-08 12:36:23 +0200

    app.js routes structure adjustments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,55 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import AuthPage from "pages/authPage";
 import NotFoundPage from "pages/notFound";
-import protectedRoutes from "utils";
+import AccountPage from "pages/accountPage";
+import HealthPage from "pages/healthPage";
+import ExpensesPage from "pages/expensesPage";
+import PetsPage from "pages/petsPage";
+
+const AuthGuard = ({ children }) => {
+    const isAuth = false;
+    return isAuth ? children : <Navigate to="/" />;
+};
 
 function App() {
-
-    const isAuth = false
-
     return (
         <div className="app">
             <BrowserRouter>
                 <Routes>
                     <Route path="*" element={<NotFoundPage />} />
-                    <Route path="/" element={isAuth ? <Navigate to="account" /> : <AuthPage />} />
-                    {protectedRoutes.map(({ path, element }) => (
-                        <Route
-                            key={path}
-                            path={path}
-                            element={isAuth ? element : <Navigate to="/" replace={true} />}
-                        />
-                    ))}
+                    <Route path="/" element={<AuthPage />} />
+                    <Route
+                        path="/account"
+                        element={
+                            <AuthGuard>
+                                <AccountPage />
+                            </AuthGuard>
+                        }
+                    />
+                    <Route
+                        path="/health"
+                        element={
+                            <AuthGuard>
+                                <HealthPage />
+                            </AuthGuard>
+                        }
+                    />
+                    <Route
+                        path="/expenses"
+                        element={
+                            <AuthGuard>
+                                <ExpensesPage />
+                            </AuthGuard>
+                        }
+                    />
+                    <Route
+                        path="/pets"
+                        element={
+                            <AuthGuard>
+                                <PetsPage />
+                            </AuthGuard>
+                        }
+                    />
                 </Routes>
             </BrowserRouter>
         </div>
